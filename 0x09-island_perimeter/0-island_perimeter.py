57a1367fbf80a7c922145f3377c7a3a8055599df
ngarukaalx <hiramkabuagi@gmail.com>
2024-05-09 11:45:14 +0000

    check with different alogarithm

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,51 +1,33 @@
 #!/usr/bin/python3
-"""This module contains fuction that gets the
-perimeter of an highland
-"""
+"""This module contains island perimeter function"""
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in a grid
-    grid is a list of list of intergers
-    """
-    if grid is None:
-        return
-
+    """Returns the perimeter of the island described in a grid."""
     num_rows = len(grid)
     num_cols = len(grid[0])
 
-    if num_rows > 100 or num_cols > 100:
-        return
-
-    # initialize perimeter to zero
     perimeter = 0
 
-    # iterate through rows:
     for i in range(num_rows):
-        # iterate through elements
         for j in range(num_cols):
-            current_element = grid[i][j]
+            if grid[i][j] == 1:
+                perimeter += 4
 
-            # check if the current element is 1 rep land
-            if current_element == 1:
-                # check left
-                if j > 0:
-                    # check prev element is 0 means water
-                    if grid[i][j - 1] == 0:
-                        perimeter += 1
-                # check top
-                if i > 0:
-                    # check if top element is 0
-                    if grid[i - 1][j] == 0:
-                        perimeter += 1
-                # check right
-                if j < num_cols - 1:
-                    # check if is zero
-                    if grid[i][j + 1] == 0:
-                        perimeter += 1
-                # check bottom
-                if i < num_rows - 1:
-                    # check if the bottom of current element is zero
-                    if grid[i + 1][j] == 0:
-                        perimeter += 1
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+
+                # Check right neighbor
+                if j < num_cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+                # Check bottom neighbor
+                if i < num_rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+
     return perimeter
---
+++ 0x09-island_perimeter/1-island_perimeter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module contains fuction that gets the
+perimeter of an highland
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in a grid
+    grid is a list of list of intergers
+    """
+    if grid is None:
+        return
+
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    if num_rows > 100 or num_cols > 100:
+        return
+
+    # initialize perimeter to zero
+    perimeter = 0
+
+    # iterate through rows:
+    for i in range(num_rows):
+        # iterate through elements
+        for j in range(num_cols):
+            current_element = grid[i][j]
+
+            # check if the current element is 1 rep land
+            if current_element == 1:
+                # check left
+                if j > 0:
+                    # check prev element is 0 means water
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                # check top
+                if i > 0:
+                    # check if top element is 0
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                # check right
+                if j < num_cols - 1:
+                    # check if is zero
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                # check bottom
+                if i < num_rows - 1:
+                    # check if the bottom of current element is zero
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+    return perimeter
